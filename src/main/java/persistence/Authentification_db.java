7467e04ad41da97c737bfec6e89b64624ca3dba7
Thomas Lecointre <thomas.lecointre1@gmail.com>
2018-02-05 17:21:54 +0100

    Authentication

--- src/main/java/persistence/Authentification_db.java
+++ src/main/java/persistence/Authentification_db.java
@@ -8,28 +8,30 @@ import java.sql.Statement;
 
 public class Authentification_db {
 
-	static Connection conn = null;
-	static String url = "jdbc:sqlite:" + System.getProperty("user.dir") + "/../lib/databases/Authentification_db";
-	
-	public Authentification_db(String url) {
-	}
-	private static void connect() throws SQLException {
+    static Connection conn = null;
+    static String url = "jdbc:sqlite:" + System.getProperty("user.dir") + "/../lib/databases/Authentification_db";
 
-			conn = DriverManager.getConnection(url);
-	}
+    public Authentification_db(String url) {
+    }
 
-	public static LogUser getLogUser(String username,String password) throws SQLException {
-		LogUser result = null;
-		connect();
-				Statement statement =conn.createStatement();
-				ResultSet resultat = statement.executeQuery("SELECT * FROM Account WHERE username='"+username+"' and password='"+password+"' ;");
-				while(resultat.next()){
-					result=(new LogUser(resultat.getString("username"),resultat.getString("password"),resultat.getString("role")));
-				}
-		disconnect();
-		return result;
-	}
-	private static void disconnect() throws SQLException {
-			conn.close();
-	}
+    private static void connect() throws SQLException {
+
+        conn = DriverManager.getConnection(url);
+    }
+
+    public static LogUser getLogUser(String username, String password) throws SQLException {
+        LogUser result = null;
+        connect();
+        Statement statement = conn.createStatement();
+        ResultSet resultat = statement.executeQuery("SELECT * FROM Account WHERE username='" + username + "' and password='" + password + "' ;");
+        while (resultat.next()) {
+            result = (new LogUser(resultat.getString("username"), resultat.getString("password"), resultat.getString("role")));
+        }
+        disconnect();
+        return result;
+    }
+
+    private static void disconnect() throws SQLException {
+        conn.close();
+    }
 }
--- src/main/java/services/SignIn.java
+++ src/main/java/services/SignIn.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.SQLException;
 import java.time.Instant;
 import java.util.Date;
 
@@ -22,7 +23,7 @@ public class SignIn {
 
     @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public Response verifyLogin(@QueryParam("username") String username, @QueryParam("password") String password) {
+    public Response verifyLogin(@QueryParam("username") String username, @QueryParam("password") String password) throws SQLException {
 
         if (username == null) {
             return Response.status(400).build();
