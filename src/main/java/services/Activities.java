5c0cd4f8d5bada41090a838a58e0d04024adc47b
Thomas Lecointre <thomas.lecointre1@gmail.com>
2018-02-06 15:39:56 +0100

    activities

--- src/main/java/services/Activities.java
+++ src/main/java/services/Activities.java
@@ -1,11 +1,16 @@
 package services;
 
+import com.google.gson.Gson;
 import filters.JWTTokenNeeded;
 import persistence.Activities_db;
+import persistence.Activity;
+import persistence.Session;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+import java.util.List;
 
 @Path("/activities")
 public class Activities {
@@ -13,23 +18,34 @@ public class Activities {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response listActivities() {
-        //Activities_db.getAllSession();
-        return null;
+        List<Session> sessions = Activities_db.getAllSessions();
+        if (sessions.isEmpty()) {
+            return Response.status(Response.Status.NO_CONTENT).entity("No sessions found.").build();
+        } else {
+            return Response.status(Response.Status.FOUND).entity(new Gson().toJson(sessions)).build();
+        }
     }
 
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getActivityFromId(@PathParam("id") String id) {
-
-        return null;
+        try {
+            Activity activity = Activities_db.getActivity(Integer.parseInt(id));
+            if (activity != null) {
+                return Response.status(Response.Status.FOUND).entity(new Gson().toJson(activity)).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).entity("No activity found at for this id.").build();
+            }
+        } catch (SQLException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
     }
 
     @GET
     @Path("/{id}/properties")
     @Produces(MediaType.APPLICATION_JSON)
     public Response listPropertiesFromIdentifiedActivity(@PathParam("id") String id) {
-        // TODO
         return null;
     }
 
